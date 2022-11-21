75d644766f8c8ed75de7ecad344a923fecd9c648
Johan <jsbollll@gmail.com>
2022-11-21 23:47:29 +0100

    done with parsing

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 set CLANG=clang++
-set FLAGS=-O0 -D _DEBUG -g -gcodeview -Wall -Wpedantic -Wno-c++20-designator
+set FLAGS=-O0 -D _DEBUG -g -gcodeview -Wall -Wpedantic -Wextra -Wconversion -Wno-missing-braces -Wno-c++20-designator
 
 set WINSOCKLIB=ws2_32
 
@@ -11,6 +11,6 @@ if not exist build mkdir build
 
 cd ./build
 
-%CLANG% ../src/*.cpp -l %WINSOCKLIB% %FLAGS% -o chat.exe && echo Compilation succeeded
+%CLANG% ../src/*.cpp -l %WINSOCKLIB% %FLAGS% -o chat.exe
 
 cd ..
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,243 @@
 
 
 #include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef uint8_t U8;
+typedef uint16_t U16;
+typedef uint32_t U32;
+typedef uint64_t U64;
+typedef size_t Usize;
+
+typedef int8_t I8;
+typedef int16_t I16;
+typedef int32_t I32;
+typedef int64_t I64;
+
+#define DEBUG_ASSERTS
+
+#ifdef DEBUG_ASSERTS
+#define assert(expression)                                                                                 \
+    do                                                                                                     \
+    {                                                                                                      \
+        if (!(expression))                                                                                 \
+        {                                                                                                  \
+            fprintf(stderr, "ERROR: assertion failed %s, at %d in %s\n", #expression, __LINE__, __FILE__); \
+            exit(1);                                                                                       \
+        }                                                                                                  \
+    } while (0)
+#else
+#define assert(expression)
+#endif
+
+#define TODO(string)                                                                   \
+    do                                                                                 \
+    {                                                                                  \
+        fprintf(stderr, "ERROR: TODO %s, at %d in %s\n", string, __LINE__, __FILE__); \
+        exit(1);                                                                       \
+    } while (0)
+
+
+
+template <typename T>
+static T *alloc(Usize amount)
+{
+    return (T *)malloc(sizeof(T) * amount);
+}
+
+
+enum class FLAGS
+{
+    HOST,
+    PORT,
+    IP,
+};
+static const Usize flag_count = 3;
+
+struct FlagData
+{
+    FLAGS flag;
+
+    union
+    {
+        U64 data;
+        U8 bytes[8];
+    };
+    
+};
+
+
+
+struct Config
+{
+    bool host;
+    U32 port;
+    U8 ip_bytes[4];
+};
+
+
+
+static Config g_config = {
+    .host = false,
+    .port = 25565,
+    .ip_bytes = {127, 0, 0, 1},
+}; 
+
+
+
+static bool is_str(const char *str1, const char *str2)
+{
+    if (str1 == nullptr || str2 == nullptr)
+    {
+        return false;
+    }
+
+    while (*str1 != '\0' && *str2 != '\0')
+    {
+        if (*str1 != *str2)
+        {
+            return false;
+        }
+        str1 += 1;
+        str2 += 1;
+    }
+    
+    return true;
+}
+
+
+static bool is_number(char chr)
+{
+    switch (chr)
+    {
+        case '0': return true;
+        case '1': return true;
+        case '2': return true;
+        case '3': return true;
+        case '4': return true;
+        case '5': return true;
+        case '6': return true;
+        case '7': return true;
+        case '8': return true;
+        case '9': return true;
+        
+        default: return false;
+    }
+}
+
+
+static Usize str_len(const char *str)
+{
+    Usize count = 0;
+    while (str[count] != '\0')
+    {
+        count += 1;
+    }
+    return count;
+}
 
 
 
 
+static FlagData parse_flag_argument(const char *)
+{
+    return FlagData();
+}
 
 
 
 int main (int argc, char *argv[])
 {
-    printf("Hello, World!");
-    
+    const char *program = argv[0]; (void)program;
+
+    // initalize config
+    for (I32 i = 1; i < argc; ++i)
+    {
+        if (argv[i][0] == '-')
+        {
+            if (is_str(&argv[i][1], "h"))
+            {
+                g_config.host = true;
+                continue;
+            }
+            else if (is_str(&argv[i][1], "ip"))
+            {
+                if (str_len(argv[i + 1]) < 7)
+                {
+                    TODO("handle ip must be longer");
+                }
+                char str_int_buffer[4];
 
 
+                I32 j = 0;
+                for (I32 bytes = 0; bytes < 4; ++bytes)
+                {
+                    if (!is_number(argv[i + 1][j]))
+                    {
+                        TODO("handle error has to be number after .");
+                    }
+
+                    I32 k = 0;
+                    while (is_number(argv[i + 1][j]))
+                    {
+                        if (k > 2)
+                        {
+                            TODO("handle error more than 3 digits in ip byte");
+                        }
+
+                        str_int_buffer[k] = argv[i + 1][j];
+                        k += 1;
+                        j += 1;
+                    }
+                    if (!is_str(&argv[i + 1][j], "."))
+                    {
+                        TODO("handle error ip bytes have to be separated by .");
+                    }
+                    j += 1;
+
+                    str_int_buffer[3] = '\0';
+                    g_config.ip_bytes[bytes] = (U8)atoi(str_int_buffer);
+                    memset(str_int_buffer, 0, sizeof(str_int_buffer));
+                }
+                i += 1;
+                continue;
+            }
+            else if (is_str(&argv[i][1], "p"))
+            {
+                Usize length = str_len(argv[i + 1]);
+                if (length == 0 || length > 5)
+                {
+                    TODO("Handle port length error");
+                }
+                U32 result;
+                {
+                    char str_int_buffer[6];
+                    for (I32 j = 0; j < 5 && argv[i + 1][j] != '\0'; ++j)
+                    {
+                        if (!is_number(argv[i + 1][j]))
+                        {
+                            TODO("Handle \"Port must a number error\"");
+                        }
+                        str_int_buffer[j] = argv[i + 1][j]; 
+                    }
+                    str_int_buffer[5] = '\0';
+                    result = (U32)atoi(str_int_buffer);
+                }
+
+                if (result < 0 || result > 65535)
+                {
+                    TODO("handle invalid port");
+                }
+                g_config.port = result;
+                i += 1;
+                continue;
+            }
+        }
+    }
+
+    printf("Config [Host: %u, port: %u, Ip: %u.%u.%u.%u]\n", g_config.host, g_config.port, g_config.ip_bytes[0], g_config.ip_bytes[1], g_config.ip_bytes[2], g_config.ip_bytes[3]);
     
     return 0;
 } 
